f34638af7a834813f9ad0dadd43c0cf39cf59ba1
aapsi <aapsi001@gmail.com>
2024-05-25 15:36:09 +0530

    init

---
+++ Frontend/README.md
@@ -0,0 +1,5 @@
+# Modern UI/UX website using React.js & Tailwind CSS
+## visit: https://bank-business-landing-page.vercel.app/
+![HooBank](https://i.ibb.co/BK1Hn0x/Screenshot-2022-08-08-at-4-05-48-PM.png)
+
+
---
+++ Frontend/src/components/Circle.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../style.css';
+
+const Circle = () => {
+  return (
+    <>
+      <ol reversed className="paces" lang="en">
+        <li className="pace js">
+          JS+
+          <blockquote className="quote js">
+            JS+: The JavaScript ecosystem (not the language!): Anyone else feels overwhelmed by the rate of rapid changes here?
+          </blockquote>
+        </li>
+        <li className="pace css">
+          CSS
+          <blockquote className="quote css">
+            CSS: This is moving in a nice click lately, with lots of useful new stuff enabled in the browsers.
+          </blockquote>
+        </li>
+        <li className="pace html">
+          HTML
+          <blockquote className="quote html">
+            HTML: Started with 20 something "tags", now we have over 100 "elements", but I feel that I've been able to keep up with the pace of change here.
+          </blockquote>
+        </li>
+        <li className="pace url">
+          URLs
+          <blockquote className="quote url">
+            URLs: Sadly they are frequently changing; links die. We should work hard to keep URLs unchanged, but that's not how it is.
+          </blockquote>
+        </li>
+        <li className="pace http">
+          HTTP
+          <blockquote className="quote http">
+            HTTP: Gradually changed (http/2), but it kind of feels good that it doesn't constantly change underneath us.
+          </blockquote>
+        </li>
+        <li className="pace tcpip">
+          TCP/IP
+          <blockquote className="quote tcpip">
+            TCP/IP: Deliberately dumb, simple, all it does is move packets around.
+          </blockquote>
+        </li>
+      </ol>
+      <footer>
+        <blockquote>
+          <p>Fast gets all the attention,<br />
+            but Slow has all the power
+          </p>
+          <cite>Stewart Brand "Pacelayers"</cite>
+        </blockquote>
+        <a href="https://vimeo.com/373128517">The Layers of the Web - Jeremy Keith</a>
+      </footer>
+    </>
+  );
+}
+
+export default Circle;
